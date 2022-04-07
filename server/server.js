6665118d392768f3e6fdcf9f8535455772d958b4
Anthony Valdez <avaldez520@gmail.com>
2022-04-07 17:48:55 -0400

    update

--- server/server.js
+++ server/server.js
@@ -17,11 +17,13 @@ const activeRooms = {};
 app.use(express.json());
 app.use(express.static(path.resolve(DIST_DIR, '../dist')));
 
-app.get('/wordle', (req, res) => {
-    res.send(gameLogic.test('therooms', req.body.guess));
-})
-
+// app.get('/wordle', (req, res) => {
+//     res.send(gameLogic.test('therooms', req.body.guess));
+// })
 app.get('/', (req, res) => {
+    res.status(200).send('Under construction');
+})
+app.get('/wardle', (req, res) => {
     res.sendFile(path.resolve(DIST_DIR, '../dist/index.html'));
 });
 
